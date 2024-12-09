1329c0ee177aefd1ec09ce8511c6209c42c01bc6
Massika-Ke <wamwoka99@gmail.com>
2024-12-09 23:55:56 +0300

    Initial commit

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState } from 'react'
+import styles from './chatList.module.css'
 
-const chatList = () => {
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+  
   return (
-    <div>chatList</div>
+    <div className={styles.chatList}>
+      <div className={styles.search}>
+        <div className={styles.searchBar}>
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search' />
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className={styles.add} 
+        onClick={()=> setAddMode((prev)=>!prev)}/>
+      </div>
+
+      <div className={styles.item}>
+        <img src="./avatar.png" alt="" className={styles.display}/>
+        <div className={styles.texts}>
+          <span>Massika Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <img src="./avatar.png" alt="" />
+        <div className={styles.texts}>
+          <span>Massika Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <img src="./avatar.png" alt="" />
+        <div className={styles.texts}>
+          <span>Massika Joe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default chatList;
+export default ChatList;
--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './userInfo.module.css'
 
 const userInfo = () => {
   return (
-    <div>userInfo</div>
+    <div className={styles.userInfo}>
+        <div className={styles.user}>
+            <img src="./avatar.png" alt="" />
+            <h2>Massika Joe</h2>
+        </div>
+        <div className={styles.icons}>
+            <img src="./more.png" alt="" />
+            <img src="./video.png" alt="" />
+            <img src="./edit.png" alt="" />
+        </div>
+    </div>
   )
 }
 
